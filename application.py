db7b0c1f86cd5b7e2cbc7e85259fc0428734a48e
LakshmishaGopalchar <58968895+LakshmishaGopalchar@users.noreply.github.com>
2020-06-21 11:04:33 +0400

    Update application.py

--- application.py
+++ application.py
@@ -14,6 +14,7 @@ database = 'TX_MAFFashionDW'
 #driver= '{ODBC Driver 17 for SQL Server}'
 drivers = [item for item in pyodbc.drivers()]
 driver = drivers[-1]
+print(driver)
 
 conn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';DATABASE='+database)
 cursor = conn.cursor()
